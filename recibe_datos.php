2b6c253f8195c98216f72912a4957fc648614165
aileen <aileenpadrontorres511@gmail.com>
2024-09-29 17:02:07 +0100

    Added recibe_datos logic for fato.html

--- recibe_datos.php
+++ recibe_datos.php
@@ -8,23 +8,30 @@
 <body>
     <?php
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            $nombre = isset($_POST['nombre']) ? $_POST['nombre'] : '';
-            $color = isset($_POST['color']) ? $_POST['color'] : '';
-            $inmunodeficiente = isset($_POST['inmunodeficiente']) ? "Sí" : "No";
-            $castrado = isset($_POST['castrado']) ? "Sí" : "No";
 
-            $gato = [$nombre, $color, $inmunodeficiente, $castrado];
+            if (isset($_POST['formulario'])) {
+                $formulario = $_POST['formulario'];
 
-            if ($gato[0] == "") {
-                echo "ERROR: El gato debe tener nombre.<br>";
-                echo "<a href='gato.html'>Volver al formulario</a>";
-            } else {
-                echo "DATOS CORRECTOS:<br>";
-                echo "Nombre del gato: " . htmlspecialchars($nombre) . "<br>";
-                echo "Color del gato: " . htmlspecialchars($color) . "<br>";
-                echo "¿Es inmunodeficiente?: " . $inmunodeficiente . "<br>";
-                echo "¿Está castrado?: " . $castrado . "<br>";
-            }
+                if ($formulario == 'gato') {
+                    $nombre = isset($_POST['nombre']) ? $_POST['nombre'] : '';
+                    $color = isset($_POST['color']) ? $_POST['color'] : '';
+                    $inmunodeficiente = isset($_POST['inmunodeficiente']) ? "Sí" : "No";
+                    $castrado = isset($_POST['castrado']) ? "Sí" : "No";
+
+                    $gato = [$nombre, $color, $inmunodeficiente, $castrado];
+
+                    if ($gato[0] == "") {
+                        echo "ERROR: El gato debe tener nombre.<br>";
+                        echo "<a href='gato.html'>Volver al formulario</a>";
+                    } else {
+                        echo "DATOS CORRECTOS:<br>";
+                        echo "Nombre del gato: " . htmlspecialchars($nombre) . "<br>";
+                        echo "Color del gato: " . htmlspecialchars($color) . "<br>";
+                        echo "¿Es inmunodeficiente?: " . $inmunodeficiente . "<br>";
+                        echo "¿Está castrado?: " . $castrado . "<br>";
+                    }
+                }
+            }  
         }
     ?>
 </body>
