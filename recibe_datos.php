ed8c54cea61f88311a215863fe1c152a7a73fed4
MJSDaw <mjsdaw@gmail.com>
2024-09-29 17:25:28 +0100

    Arreglo formulario y completo mi parte del recibe_datos

--- recibe_datos.php
+++ recibe_datos.php
@@ -30,8 +30,33 @@
                         echo "¿Es inmunodeficiente?: " . $inmunodeficiente . "<br>";
                         echo "¿Está castrado?: " . $castrado . "<br>";
                     }
+                } elseif ($formulario == 'index'){
+                    $nombre1 = isset($_POST['nombre1']) ? $_POST['nombre1'] : '';
+                    $apellidos = isset($_POST['apellidos']) ? $_POST['apellidos'] : '';
+                    $coche = isset($_POST['coche']) ? 'Si' : 'No';
+                    $moto = isset($_POST['moto']) ? 'Si' : 'No';
+                    $barco = isset($_POST['barco']) ? 'Si' : 'No';
+                    $comida = isset($_POST['comida']) ? $_POST['comida'] : '';
+
+                    $persona = [$nombre1, $apellidos, $coche, $moto, $barco, $comida];
+
+                    if ($persona[0] == '') {
+                        echo "ERROR: Debe introducir un nombre.<br>";
+                        echo "<a href='index.html'>Volver al formulario</a>";
+                    } elseif ($persona[1] == '') {
+                        echo "ERROR: Debe introducir apellidos.<br>";
+                        echo "<a href='index.html'>Volver al formulario</a>";
+                    } else {
+                        echo "DATOS CORRECTOS:<br>";
+                        echo "Nombre: " . htmlspecialchars($nombre1) . "<br>";
+                        echo "Apellidos: " . htmlspecialchars($apellidos) . "<br>";
+                        echo "¿Tiene coche?: " . $coche . "<br>";
+                        echo "¿Tiene moto?: " . $moto . "<br>";
+                        echo "¿Tiene barco?: " . $barco . "<br>";
+                        echo "Comida escogida: " . $comida;
+                    }
                 }
-            }  
+            } 
         }
     ?>
 </body>
